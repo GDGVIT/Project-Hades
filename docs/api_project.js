6d8cab9b70b65d63e50e89d54319c9a8201f7cf8
angadsharma1016 <angadsharma1016@gmail.com>
2019-01-26 20:20:47 +0530

    event docs

---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "Project Hades documentation",
+  "version": "0.1.0",
+  "description": "The last event management tool you will ever need",
+  "title": "Custom apiDoc browser title",
+  "url": "http://localhost:8800",
+  "sampleUrl": "http://localhost:8800",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-01-26T14:49:39.560Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
