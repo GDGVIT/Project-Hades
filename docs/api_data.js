c589e203d32eb78f850647294632a5c632a7d134
angadsharma1016 <angadsharma1016@gmail.com>
2019-01-26 20:20:47 +0530

    event docs

---
+++ docs/api_data.js
@@ -0,0 +1,318 @@
+define({ "api": [
+  {
+    "type": "post",
+    "url": "/create-event",
+    "title": "create a new event",
+    "name": "create_a_new_event",
+    "group": "admin",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "clubName",
+            "description": "<p>Name of your club</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>Name of your event</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "toDate",
+            "description": "<p>ending date</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "fromDate",
+            "description": "<p>start date</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "toTime",
+            "description": "<p>start time</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "fromTime",
+            "description": "<p>ending time</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "budget",
+            "description": "<p>budget</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "description",
+            "description": "<p>event description</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "category",
+            "description": "<p>category of the event</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "venue",
+            "description": "<p>event venue</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "attendance",
+            "description": "<p>Name of your club</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "expectedParticipants",
+            "description": "<p>Expected Participants in the event</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Object",
+            "optional": false,
+            "field": "facultyCoordinator",
+            "description": "<p>faculty coordinator details (Participant Object)</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Object",
+            "optional": false,
+            "field": "studentCoordinator",
+            "description": "<p>student coordinator details (Participant Object)</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Object",
+            "optional": false,
+            "field": "guest",
+            "description": "<p>guest details (Guest object)</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "PROrequest",
+            "description": "<p>PRO department requests</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "campusEngineerRequest",
+            "description": "<p>Campus engineer requests</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "duration",
+            "description": "<p>duration of event</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Object",
+            "optional": false,
+            "field": "mainSponsor",
+            "description": "<p>sponsor details (Participant Object)</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "request-example",
+          "content": "\n\"event\":{\n \"clubName\":\"GDG\",\n \"name\":\"DEVRELCONF\",\n \"toDate\":\"10TH OCTOBER\",\n \"fromDate\":\"8TH OCTOBER\",\n \"toTime\":\"10 PM\",\n \"fromTime\":\"11 AM\",\n \"budget\":\"200000\",\n \"description\":\"TECHNICAL EVENT AT GDG VIT. ITS GONNA BE AMAZING\",\n \"category\":\"TECHNICAL\",\n \"venue\":\"ANNA AUDI\",\n \"attendance\":\"4000\",\n \"expectedParticipants\":\"4000\",\n \"facultyCoordinator\":{\n    \"name\":\"NOORU MAA\",\n    \"registrationNumber\":\"17BBE1010\",\n    \"email\":\"SDADAS@A.COM\",\n    \"phoneNumber\":\"919191991911\",\n    \"gender\":\"M\",\n    \"eventsAttended\":\"ALL\"\n },\n \"studentCoordinator\":{\n    \"name\":\"NOORU BAAP\",\n    \"registrationNumber\":\"17BBE1010\",\n    \"email\":\"SDADAS@A.COM\",\n    \"phoneNumber\":\"919191991911\",\n    \"gender\":\"M\",\n    \"eventsAttended\":\"ALL\"\n },\n \"guest\":{\n    \"name\":\"ALLAHH DAAS\",\n    \"email\":\"ASDSAD#ASD.COM\",\n    \"phoneNumber\":\"11111111111\",\n    \"gender\":\"F\",\n    \"stake\":\"SOME MONAYYYY\",\n    \"locationOfStay\":\"TERA GHAR\"\n },\n \"PROrequest\":\"SAJDOOSIJANDFSAKFDSAFD\",\n \"campusEngineerRequest\":\"SDFHBSADUB, ASNFD , AS KDFSAM FDSA, AS, SD\",\n \"duration\":\"16 hours\",\n \"mainSponsor\":{\n    \"name\":\"ALLAHH DAAS\",\n    \"email\":\"ASDSAD#ASD.COM\",\n    \"phoneNumber\":\"11111111111\",\n    \"gender\":\"F\",\n    \"stake\":\"SOME MONAYYYY\",\n    \"locationOfStay\":\"TERA GHAR\"\n }\n\n\napiParamExample {json} response-example\n\nrs:\"\",\nerr:null",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "./events/pkg/service/service.go",
+    "groupTitle": "admin",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:8800/create-event"
+      }
+    ]
+  },
+  {
+    "type": "post",
+    "url": "/delete-event",
+    "title": "delete an event",
+    "name": "delete_an_event",
+    "group": "admin",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "key",
+            "description": "<p>key to query the event by</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "value",
+            "description": "<p>value of the key</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "request-example",
+          "content": "{\n\t\"query\":{\n\t\t\"key\":\"clubName\",\n\t\t\"value\":\"GDG\"\n\t}\n}",
+          "type": "json"
+        },
+        {
+          "title": "response-example",
+          "content": "{\n\t rs:\"\",\n\t err:null\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "./events/pkg/service/service.go",
+    "groupTitle": "admin",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:8800/delete-event"
+      }
+    ]
+  },
+  {
+    "type": "post",
+    "url": "/read-event",
+    "title": "read an event",
+    "name": "read_an_event",
+    "group": "admin",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "key",
+            "description": "<p>key to query the event by</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "value",
+            "description": "<p>value of the key</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "request-example",
+          "content": "   \"query\":{\n\t\t\"key\":\"clubName\",\n\t\t\"value\":\"GDG\"\n\t}",
+          "type": "json"
+        },
+        {
+          "title": "response-example",
+          "content": "{\n   \"rs\": {\n       \"clubName\": \"GDG\",\n       \"name\": \"DEVRELCONF\",\n       \"toDate\": \"10TH OCTOBER\",\n       \"fromDate\": \"8TH OCTOBER\",\n       \"toTime\": \"10 PM\",\n       \"fromTime\": \"11 AM\",\n       \"budget\": \"200000\",\n       \"description\": \"TECHNICAL EVENT AT GDG VIT. ITS GONNA BE AMAZING\",\n       \"category\": \"TECHNICAL\",\n       \"venue\": \"ANNA AUDI\",\n       \"attendance\": \"4000\",\n       \"expectedParticipants\": \"4000\",\n       \"facultyCoordinator\": {\n           \"name\": \"Murali S\",\n           \"registrationNumber\": \"\",\n           \"email\": \"SDADAS@A.COM\",\n           \"phoneNumber\": \"919191991911\",\n           \"gender\": \"M\"\n       },\n       \"studentCoordinator\": {\n           \"name\": \"Dhruv sharma\",\n           \"registrationNumber\": \"17BBE1010\",\n           \"email\": \"SDADAS@A.COM\",\n           \"phoneNumber\": \"919191991911\",\n           \"gender\": \"M\"\n       },\n       \"guest\": {\n           \"name\": \"angad sharma\"\",\n           \"email\": \"ASDSAD#ASD.COM\",\n           \"phoneNumber\": \"11111111111\",\n           \"gender\": \"F\",\n           \"stake\": \"SOME MONAYYYY\",\n           \"locationOfStay\": \"VIT campus\"\n       },\n       \"PROrequest\": \"SAJDOOSIJANDFSAKFDSAFD\",\n       \"campusEngineerRequest\": \"SDFHBSADUB, ASNFD , AS KDFSAM FDSA, AS, SD\",\n       \"duration\": \"16 hours\",\n       \"mainSponsor\": {\n           \"name\": \"\",\n           \"registrationNumber\": \"\",\n           \"email\": \"\",\n           \"phoneNumber\": \"\",\n           \"gender\": \"\"\n       }\n   },\n   \"err\": null\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "./events/pkg/service/service.go",
+    "groupTitle": "admin",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:8800/read-event"
+      }
+    ]
+  },
+  {
+    "type": "post",
+    "url": "/update-event",
+    "title": "update an event",
+    "name": "update_an_event",
+    "group": "admin",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "key",
+            "description": "<p>key to query the event by</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "value",
+            "description": "<p>value of the key</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "changeKey",
+            "description": "<p>key of the value which needs to be altered</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "changeValue",
+            "description": "<p>the new value</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "request-example",
+          "content": "{\n\t\"query\":{\n\t\t\"key\":\"clubName\",\n\t\t\"value\":\"GDG\",\n\t\t\"changeKey\":\"clubName\",\n\t\t\"changeValue\":\"codechef\"\n\t}\n}",
+          "type": "json"
+        },
+        {
+          "title": "response-example",
+          "content": "{\n\trs:\"\",\n\terr:null\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "./events/pkg/service/service.go",
+    "groupTitle": "admin",
+    "sampleRequest": [
+      {
+        "url": "http://localhost:8800/update-event"
+      }
+    ]
+  }
+] });
--- events/pkg/service/service.go
+++ events/pkg/service/service.go
@@ -9,7 +9,6 @@ import (
 
 // EventsService describes the service.
 type EventsService interface {
-	// Add your methods here
 	CreateEvent(ctx context.Context, event model.Event) (rs string, err error)
 	ReadEvent(ctx context.Context, query model.Query) (rs model.Event, err error)
 	UpdateEvent(ctx context.Context, query model.Query) (rs string, err error)
@@ -18,6 +17,90 @@ type EventsService interface {
 
 type basicEventsService struct{}
 
+/**
+* @api {post} /create-event create a new event
+* @apiName create a new event
+* @apiGroup admin
+*
+* @apiParam {String} clubName Name of your club
+* @apiParam {String} name Name of your event
+* @apiParam {String} toDate ending date
+* @apiParam {String} fromDate start date
+* @apiParam {String} toTime start time
+* @apiParam {String} fromTime ending time
+* @apiParam {String} budget budget
+* @apiParam {String} description event description
+* @apiParam {String} [category] category of the event
+* @apiParam {String} venue event venue
+* @apiParam {String} [attendance] Name of your club
+* @apiParam {String} expectedParticipants Expected Participants in the event
+* @apiParam {Object} facultyCoordinator faculty coordinator details (Participant Object)
+* @apiParam {Object} studentCoordinator student coordinator details (Participant Object)
+* @apiParam {Object} guest guest details (Guest object)
+* @apiParam {String} PROrequest PRO department requests
+* @apiParam {String} campusEngineerRequest Campus engineer requests
+* @apiParam {String} duration duration of event
+* @apiParam {Object} mainSponsor sponsor details (Participant Object)
+*
+* @apiParamExample {json} request-example
+*
+*"event":{
+*  "clubName":"GDG",
+*  "name":"DEVRELCONF",
+*  "toDate":"10TH OCTOBER",
+*  "fromDate":"8TH OCTOBER",
+*  "toTime":"10 PM",
+*  "fromTime":"11 AM",
+*  "budget":"200000",
+*  "description":"TECHNICAL EVENT AT GDG VIT. ITS GONNA BE AMAZING",
+*  "category":"TECHNICAL",
+*  "venue":"ANNA AUDI",
+*  "attendance":"4000",
+*  "expectedParticipants":"4000",
+*  "facultyCoordinator":{
+*     "name":"NOORU MAA",
+*     "registrationNumber":"17BBE1010",
+*     "email":"SDADAS@A.COM",
+*     "phoneNumber":"919191991911",
+*     "gender":"M",
+*     "eventsAttended":"ALL"
+*  },
+*  "studentCoordinator":{
+*     "name":"NOORU BAAP",
+*     "registrationNumber":"17BBE1010",
+*     "email":"SDADAS@A.COM",
+*     "phoneNumber":"919191991911",
+*     "gender":"M",
+*     "eventsAttended":"ALL"
+*  },
+*  "guest":{
+*     "name":"ALLAHH DAAS",
+*     "email":"ASDSAD#ASD.COM",
+*     "phoneNumber":"11111111111",
+*     "gender":"F",
+*     "stake":"SOME MONAYYYY",
+*     "locationOfStay":"TERA GHAR"
+*  },
+*  "PROrequest":"SAJDOOSIJANDFSAKFDSAFD",
+*  "campusEngineerRequest":"SDFHBSADUB, ASNFD , AS KDFSAM FDSA, AS, SD",
+*  "duration":"16 hours",
+*  "mainSponsor":{
+*     "name":"ALLAHH DAAS",
+*     "email":"ASDSAD#ASD.COM",
+*     "phoneNumber":"11111111111",
+*     "gender":"F",
+*     "stake":"SOME MONAYYYY",
+*     "locationOfStay":"TERA GHAR"
+*  }
+*
+*
+*apiParamExample {json} response-example
+*
+*rs:"",
+*err:null
+*
+*
+ */
 func (b *basicEventsService) CreateEvent(ctx context.Context, event model.Event) (rs string, err error) {
 
 	// create connection to DB
@@ -36,57 +119,70 @@ func (b *basicEventsService) CreateEvent(ctx context.Context, event model.Event)
 	return rs, err
 }
 
-/*
-{
-    "rs": {
-        "clubName": "GDG",
-        "name": "DEVRELCONF",
-        "toDate": "10TH OCTOBER",
-        "fromDate": "8TH OCTOBER",
-        "toTime": "10 PM",
-        "fromTime": "11 AM",
-        "budget": "200000",
-        "description": "TECHNICAL EVENT AT GDG VIT. ITS GONNA BE AMAZING",
-        "category": "TECHNICAL",
-        "venue": "ANNA AUDI",
-        "attendance": "4000",
-        "expectedParticipants": "4000",
-        "facultyCoordinator": {
-            "name": "Murali S",
-            "registrationNumber": "",
-            "email": "SDADAS@A.COM",
-            "phoneNumber": "919191991911",
-            "gender": "M"
-        },
-        "studentCoordinator": {
-            "name": "Dhruv sharma",
-            "registrationNumber": "17BBE1010",
-            "email": "SDADAS@A.COM",
-            "phoneNumber": "919191991911",
-            "gender": "M"
-        },
-        "guest": {
-            "name": "angad sharma"",
-            "email": "ASDSAD#ASD.COM",
-            "phoneNumber": "11111111111",
-            "gender": "F",
-            "stake": "SOME MONAYYYY",
-            "locationOfStay": "VIT campus"
-        },
-        "PROrequest": "SAJDOOSIJANDFSAKFDSAFD",
-        "campusEngineerRequest": "SDFHBSADUB, ASNFD , AS KDFSAM FDSA, AS, SD",
-        "duration": "16 hours",
-        "mainSponsor": {
-            "name": "",
-            "registrationNumber": "",
-            "email": "",
-            "phoneNumber": "",
-            "gender": ""
-        }
-    },
-    "err": null
-}
-*/
+/**
+*@api {post} /read-event read an event
+*@apiName read an event
+*@apiGroup admin
+*@apiParam {String} key key to query the event by
+*@apiParam {String} value value of the key
+*
+*@apiParamExample {json} request-example
+*    "query":{
+*		"key":"clubName",
+*		"value":"GDG"
+*	}
+*
+*@apiParamExample {json} response-example
+*{
+*    "rs": {
+*        "clubName": "GDG",
+*        "name": "DEVRELCONF",
+*        "toDate": "10TH OCTOBER",
+*        "fromDate": "8TH OCTOBER",
+*        "toTime": "10 PM",
+*        "fromTime": "11 AM",
+*        "budget": "200000",
+*        "description": "TECHNICAL EVENT AT GDG VIT. ITS GONNA BE AMAZING",
+*        "category": "TECHNICAL",
+*        "venue": "ANNA AUDI",
+*        "attendance": "4000",
+*        "expectedParticipants": "4000",
+*        "facultyCoordinator": {
+*            "name": "Murali S",
+*            "registrationNumber": "",
+*            "email": "SDADAS@A.COM",
+*            "phoneNumber": "919191991911",
+*            "gender": "M"
+*        },
+*        "studentCoordinator": {
+*            "name": "Dhruv sharma",
+*            "registrationNumber": "17BBE1010",
+*            "email": "SDADAS@A.COM",
+*            "phoneNumber": "919191991911",
+*            "gender": "M"
+*        },
+*        "guest": {
+*            "name": "angad sharma"",
+*            "email": "ASDSAD#ASD.COM",
+*            "phoneNumber": "11111111111",
+*            "gender": "F",
+*            "stake": "SOME MONAYYYY",
+*            "locationOfStay": "VIT campus"
+*        },
+*        "PROrequest": "SAJDOOSIJANDFSAKFDSAFD",
+*        "campusEngineerRequest": "SDFHBSADUB, ASNFD , AS KDFSAM FDSA, AS, SD",
+*        "duration": "16 hours",
+*        "mainSponsor": {
+*            "name": "",
+*            "registrationNumber": "",
+*            "email": "",
+*            "phoneNumber": "",
+*            "gender": ""
+*        }
+*    },
+*    "err": null
+*}
+**/
 func (b *basicEventsService) ReadEvent(ctx context.Context, query model.Query) (rs model.Event, err error) {
 	// create connection to DB
 	conn := model.ConnectToDB(fmt.Sprintf("bolt://%s:%s@%s",
@@ -107,21 +203,35 @@ func (b *basicEventsService) ReadEvent(ctx context.Context, query model.Query) (
 	return cb.Event, err
 }
 
-/*
-{
-	"query":{
-		"key":"clubName",
-		"value":"GDG",
-		"changeKey":"clubName",
-		"changeValue":"codechef"
-	}
-}
-*/
+/**
+*@api {post} /update-event update an event
+*@apiName update an event
+*@apiGroup admin
+*@apiParam {String} key key to query the event by
+*@apiParam {String} value value of the key
+*@apiParam {String} changeKey key of the value which needs to be altered
+*@apiParam {String} changeValue the new value
+*
+*@apiParamExample {json} request-example
+*{
+*	"query":{
+*		"key":"clubName",
+*		"value":"GDG",
+*		"changeKey":"clubName",
+*		"changeValue":"codechef"
+*	}
+*}
+*@apiParamExample {json} response-example
+*{
+*	rs:"",
+*	err:null
+*}
+**/
 func (b *basicEventsService) UpdateEvent(ctx context.Context, query model.Query) (rs string, err error) {
 	// create connection to DB
 	conn := model.ConnectToDB(fmt.Sprintf("bolt://%s:%s@%s",
 		model.DB_SECRET.DB_USERNAME, model.DB_SECRET.DB_PASSWORD,
-		model.DB_SECRET.DB_ENDPOINT)) //("bolt://username:password@localhost:7687"
+		model.DB_SECRET.DB_ENDPOINT))
 	fmt.Println(conn)
 
 	defer conn.Close()
@@ -135,14 +245,27 @@ func (b *basicEventsService) UpdateEvent(ctx context.Context, query model.Query)
 	return rs, err
 }
 
-/*
-{
-	"query":{
-		"key":"clubName",
-		"value":"GDG"
-	}
-}
-*/
+/**
+*@api {post} /delete-event delete an event
+*@apiName delete an event
+*@apiGroup admin
+*@apiParam {String} key key to query the event by
+*@apiParam {String} value value of the key
+*
+*@apiParamExample {json} request-example
+*{
+*	"query":{
+*		"key":"clubName",
+*		"value":"GDG"
+*	}
+*}
+*@apiParamExample {json} response-example
+*{
+*	 rs:"",
+*	 err:null
+* }
+*
+**/
 func (b *basicEventsService) DeleteEvent(ctx context.Context, query model.Query) (rs string, err error) {
 	// create connection to DB
 	conn := model.ConnectToDB(fmt.Sprintf("bolt://%s:%s@%s",
