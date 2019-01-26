2345a33e367f4192e4255075118b803aabe1e574
angadsharma1016 <angadsharma1016@gmail.com>
2019-01-26 20:24:01 +0530

    event docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-26T14:49:39.560Z",
+    "time": "2019-01-26T14:53:54.962Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
