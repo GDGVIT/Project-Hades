96bf31fef5d630fec0bfe2bb090d1cce0136fa77
angadsharma1016 <angadsharma1016@gmail.com>
2019-02-03 04:20:50 +0530

    view present, absent Used mutexes for concurrency to find absentees

--- simple_projection/pkg/service/service.go
+++ simple_projection/pkg/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"sync"
 
 	"github.com/GDGVIT/Project-Hades/model"
 )
@@ -25,64 +26,65 @@ type basicSimpleProjectionService struct{}
 *@apiParam {String} event event name
 *
 *@apiParamExample {json} request-example
-{
-	"event":"DEVFEST 2019",
-	"query":{
-		"key":"gender",
-		"value":"F"
-	}
-}
+*{
+*	"event":"DEVFEST 2019",
+*	"query":{
+*		"key":"gender",
+*		"value":"F"
+*	}
+*}
 *@apiParamExample {json} response-example
-{
-    "rs": [
-        {
-            "name": "das",
-            "registrationNumber": "17BCE2009",
-            "email": "x@axd.com",
-            "phoneNumber": "919191991911",
-            "gender": "F"
-        }
-    ],
-    "err": null
-}
-
+*{
+*    "rs": [
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@axd.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "F"
+*        }
+*    ],
+*    "err": null
+*}
+*
 *@apiParamExample {json} request-example-all
-{
-	"event":"DEVFEST 2019"
-}
-
+*{
+*	"event":"DEVFEST 2019"
+*}
+*
 *@apiParamExample {json} response-example-all
-{
-    "rs": [
-        {
-            "name": "das",
-            "registrationNumber": "17BCE2009",
-            "email": "x@axd.com",
-            "phoneNumber": "919191991911",
-            "gender": "F"
-        },
-        {
-            "name": "das",
-            "registrationNumber": "17BCE2009",
-            "email": "x@ax.com",
-            "phoneNumber": "919191991911",
-            "gender": "M"
-        },
-        {
-            "name": "das",
-            "registrationNumber": "17BCE2009",
-            "email": "x@x.com",
-            "phoneNumber": "919191991911",
-            "gender": "M"
-        }
-    ],
-    "err": null
-}
+*{
+*    "rs": [
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@axd.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "F"
+*        },
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@ax.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "M"
+*        },
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@x.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "M"
+*        }
+*    ],
+*    "err": null
+*}
 **/
 func (b *basicSimpleProjectionService) ProjectAll(ctx context.Context, event string, query model.Query) (rs []model.Participant, err error) {
 
 	c := make(chan model.SafeParticipantReturn)
-	go model.ViewAll(event, query, c)
+	mutex := &sync.Mutex{}
+	go model.ViewAll(event, query, c, mutex)
 
 	msg := <-c
 	if err := msg.Err; err != nil {
@@ -91,12 +93,132 @@ func (b *basicSimpleProjectionService) ProjectAll(ctx context.Context, event str
 	return msg.Participants, msg.Err
 
 }
+
+/**
+*@api {post} /project-present show present participants
+*@apiName show present participants
+*@apiGroup simple_projection
+*@apiPermission admin
+*@apiParam {String} [key] key to query the event by
+*@apiParam {String} [value] value of the key
+*@apiParam {String} event event name
+*@apiParam {String} day day of the event
+*
+*@apiParamExample {json} request-example
+*{
+*	"event":"DEVFEST 2019",
+*	"day":2,
+*	"query":{
+*		"key":"gender",
+*		"value":"F"
+*	}
+*}
+*@apiParamExample {json} response-example
+*{
+*    "rs": [
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@axd.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "F"
+*        }
+*    ],
+*    "err": null
+*}
+*
+*@apiParamExample {json} request-example-all
+*{
+*	"event":"DEVFEST 2019",
+*	"day":2
+*}
+*
+*@apiParamExample {json} response-example-all
+*{
+*    "rs": [
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@axd.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "F"
+*        },
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@ax.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "M"
+*        },
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@x.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "M"
+*        }
+*    ],
+*    "err": null
+*}
+**/
 func (b *basicSimpleProjectionService) ProjectPresent(ctx context.Context, event string, day int, query model.Query) (rs []model.Participant, err error) {
-	// TODO implement the business logic of ProjectPresent
+
+	c := make(chan model.SafeParticipantReturn)
+	mutex := &sync.Mutex{}
+	go model.ViewPresent(event, query, day, c, mutex)
+
+	msg := <-c
+	if err := msg.Err; err != nil {
+		return nil, err
+	}
+	return msg.Participants, msg.Err
 	return rs, err
 }
+
+/**
+*@api {post} /project-absent show absent participants
+*@apiName show absent participants
+*@apiGroup simple_projection
+*@apiPermission admin
+*@apiParam {String} [key] key to query the event by
+*@apiParam {String} [value] value of the key
+*@apiParam {String} event event name
+*@apiParam {String} day day of the event
+*
+*@apiParamExample {json} request-example
+*{
+*	"event":"DEVFEST 2019",
+*	"day":2,
+*	"query":{
+*		"key":"gender",
+*		"value":"F"
+*	}
+*}
+*@apiParamExample {json} response-example
+*{
+*    "rs": [
+*        {
+*            "name": "das",
+*            "registrationNumber": "17BCE2009",
+*            "email": "x@axd.com",
+*            "phoneNumber": "919191991911",
+*            "gender": "F"
+*        }
+*    ],
+*    "err": null
+*}
+*
+*}
+**/
 func (b *basicSimpleProjectionService) ProjectAbsent(ctx context.Context, event string, day int, query model.Query) (rs []model.Participant, err error) {
-	// TODO implement the business logic of ProjectAbsent
+	c := make(chan model.SafeParticipantReturn)
+	go model.ViewAbsent(event, query, day, c)
+
+	msg := <-c
+	if err := msg.Err; err != nil {
+		return nil, err
+	}
+	return msg.Participants, msg.Err
 	return rs, err
 }
 
