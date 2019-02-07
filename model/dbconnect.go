c98a9c37eb156547a501b26960716fbdc24cccaa
angadsharma1016 <angadsharma1016@gmail.com>
2019-02-08 00:27:02 +0530

    environment setup
    1. single dotenv
    2. imported into containers during runtime
    3. exporter init

--- model/dbconnect.go
+++ model/dbconnect.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
-	"github.com/joho/godotenv"
 )
 
 var con bolt.Conn
@@ -15,19 +14,6 @@ func DBInit(c bolt.Conn) {
 }
 
 func ConnectToDB() bolt.Conn {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	// cmd := exec.Command("sleep", "10")
-	// cmd.Stdout = os.Stdout
-	// cmd.Stderr = os.Stderr
-	// err = cmd.Run()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// }
 
 	conn, err := bolt.NewDriver().OpenNeo(os.Getenv("PROD_URI"))
 	if err != nil {
