87f04ef7b273b09844bbc223e89381f6f507c55c
angadsharma1016 <angadsharma1016@gmail.com>
2019-02-17 14:54:06 +0530

    immutable eventStore
    1. sqlite3 as eventStore
    2. Only create and read funcs
    3. Used GORM go object relational model

--- analytics/cmd/main.go
+++ analytics/cmd/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/GDGVIT/Project-Hades/analytics/endpoints"
+import (
+	"github.com/GDGVIT/Project-Hades/analytics/db"
+	"github.com/GDGVIT/Project-Hades/analytics/endpoints"
+)
 
 func main() {
 	var server *endpoints.Server
+
+	// immutable event storage
+	conn := db.DBconnect()
+	defer conn.Close()
+
 	server.Run()
 }
