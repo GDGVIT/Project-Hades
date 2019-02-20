a9c72048335438eab84f3db702df0e6d32af017a
angadsharma1016 <angadsharma1016@gmail.com>
2019-02-20 20:06:48 +0530

    reading from logs DB
    1. using channels to read from DB

--- analytics/endpoints/handlerfuncs.go
+++ analytics/endpoints/handlerfuncs.go
@@ -1,9 +1,51 @@
 package endpoints
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-func index() http.HandlerFunc {
+	"github.com/GDGVIT/Project-Hades/analytics/db"
+)
+
+func readFromDB() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Analytics func"))
+
+		// make a map of all GET queries
+		queries := r.URL.Query()
+		var (
+			arr    [][]db.Logs
+			errs   []error
+			buflen = len(queries)
+			count  int
+		)
+
+		// make a channel with buffer length same as the number of queries
+		ch := make(chan db.LogsReturn, buflen)
+		for key, value := range queries {
+			go db.ReadLogs(key, value[0], ch)
+		}
+
+		// query channel buffers, then close channel
+		for i := range ch {
+			count++
+			if i.Err != nil {
+				errs = append(errs, i.Err)
+			}
+			arr = append(arr, i.Logs)
+			if count == buflen {
+				break
+			}
+		}
+		close(ch)
+
+		// send JSON response
+		json.NewEncoder(w).Encode(struct {
+			Logs   [][]db.Logs `json:"logs"`
+			Errors []error     `json:"errors"`
+		}{
+			arr,
+			errs,
+		})
+
 	}
 }
