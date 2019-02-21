75a2434204d5911f62f092dff284332239328f5a
angadsharma1016 <angadsharma1016@gmail.com>
2019-02-22 03:05:09 +0530

    guest CRUD

--- guests/pkg/service/service.go
+++ guests/pkg/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"sync"
 
 	"github.com/GDGVIT/Project-Hades/model"
 )
@@ -23,20 +24,37 @@ type GuestsService interface {
 type basicGuestsService struct{}
 
 func (b *basicGuestsService) CreateGuest(ctx context.Context, event string, guest model.Guest) (rs string, err error) {
-	// TODO implement the business logic of CreateGuest
-	return rs, err
+
+	c := make(chan error)
+	mutex := &sync.Mutex{}
+	go model.CreateGuest(event, guest, c, mutex)
+	if err := <-c; err != nil {
+		return "Some error occurred while creating guest", err
+	}
+	return "Guest created", nil
 }
+
 func (b *basicGuestsService) ReadGuest(ctx context.Context, query model.Query) (rs []model.Guest, err error) {
-	// TODO implement the business logic of ReadGuest
-	return rs, err
+	c := make(chan model.GuestReturn)
+	go model.ReadGuest(query, c)
+
+	data := <-c
+	if err := data.Err; err != nil {
+		return nil, err
+	}
+	return data.Guests, nil
 }
 func (b *basicGuestsService) UpdateGuest(ctx context.Context, query model.Query) (rs string, err error) {
 	// TODO implement the business logic of UpdateGuest
 	return rs, err
 }
 func (b *basicGuestsService) DeleteGuest(ctx context.Context, query model.Query) (rs string, err error) {
-	// TODO implement the business logic of DeleteGuest
-	return rs, err
+	c := make(chan error)
+	go model.RmGuest(query, c)
+	if err := <-c; err != nil {
+		return "Error occurred while deleting guest", err
+	}
+	return "Guest deleted", nil
 }
 func (b *basicGuestsService) CreateSponsor(ctx context.Context, event string, sponsor model.Participant) (rs string, err error) {
 	// TODO implement the business logic of CreateSponsor
