45d5cf884b019110135b8030e4bc60a0c5756e58
angadsharma1016 <angadsharma1016@gmail.com>
2019-02-28 14:16:18 +0530

    create coupon schema
    1. separate node for each schema
    2. mutex locks and goroutines for fast creation

--- coupons/cmd/main.go
+++ coupons/cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import service "github.com/GDGVIT/Project-Hades/coupons/cmd/service"
+import (
+	service "github.com/GDGVIT/Project-Hades/coupons/cmd/service"
+	"github.com/GDGVIT/Project-Hades/model"
+)
 
 func main() {
+	conn := model.ConnectToDB()
+	defer conn.Close()
+	model.DBInit(conn)
 	service.Run()
 }
