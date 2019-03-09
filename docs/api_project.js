eed28891128e2a045c9ca7ea45920f81d7a991a5
angadsharma1016 <angadsharma1016@gmail.com>
2019-03-09 15:19:41 +0530

    docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-03-09T09:42:58.219Z",
+    "time": "2019-03-09T09:49:31.533Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
