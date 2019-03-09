c72be7adb597a759fa5866ed6e75d079c181c271
angadsharma1016 <angadsharma1016@gmail.com>
2019-03-09 13:30:49 +0530

    docs

--- coupons/pkg/service/service.go
+++ coupons/pkg/service/service.go
@@ -18,6 +18,47 @@ type CouponsService interface {
 
 type basicCouponsService struct{}
 
+/**
+*@api {post} /api/v1/coupons/create-schema create coupon schema
+*@apiName create coupon schema
+*@apiGroup coupons
+*@apiPermission admin
+*
+*@apiParam {string} event name of the event
+*@apiParam {string} name name of the coupon
+*@apiParam {string} description description of the coupon
+*@apiParam {int} day day of the event
+*
+*@apiParamExample {json} request-example
+*
+*{
+*	"event":"DEVRELCONF",
+*	"coupons": [{
+*		"name":"lunch",
+*		"description":"lunch",
+*		"day":1
+*	},{
+*		"name":"bf",
+*		"description":"bf",
+*		"day":1
+*	},{
+*		"name":"lunch",
+*		"description":"lunch",
+*		"day":2
+*	},{
+*		"name":"dinner",
+*		"description":"dinner",
+*		"day":2
+*	}]
+*}
+*
+*@apiParamExample {json} response-example
+*{
+*    "rs": "Successfully creted coupon schema for event DEVRELCONF",
+*    "err": null
+*}
+*
+**/
 func (b *basicCouponsService) CreateSchema(ctx context.Context, event string, coupons []model.Coupon) (rs string, err error) {
 
 	c := make(chan error)
@@ -28,6 +69,31 @@ func (b *basicCouponsService) CreateSchema(ctx context.Context, event string, co
 	return "Successfully creted coupon schema for event " + event, nil
 }
 
+/**
+*@api {post} /api/v1/coupons/mark-present mark attendee present
+*@apiName mark attendee present
+*@apiGroup coupons
+*@apiPermission admin
+*
+*@apiParam {string} eventName name of the event
+*@apiParam {string} email email of the attendee
+*@apiParam {int} day day of the event
+*
+*@apiParamExample {json} request-example
+*{
+*	"attendance":{
+*		"eventName":"DEVRELCONF",
+*		"email":"a@a.COM",
+*		"day":2
+*	}
+*}
+*
+*@apiParamExample {json} response-example
+*{
+*    "rs": "Successfully marked present for the day",
+*    "err": null
+*}
+**/
 func (b *basicCouponsService) MarkPresent(ctx context.Context, attendance model.Attendance) (rs string, err error) {
 	c := make(chan model.MessageReturn)
 	go model.MarkPresent(attendance, c)
@@ -38,6 +104,33 @@ func (b *basicCouponsService) MarkPresent(ctx context.Context, attendance model.
 	return msg.Message, nil
 }
 
+/**
+*@api {post} /api/v1/coupons/redeem-coupon redeem a coupon
+*@apiName redeem a coupon
+*@apiGroup coupons
+*@apiPermission admin
+*
+*@apiParam {string} eventName name of the event
+*@apiParam {string} email email of the attendee
+*@apiParam {int} day day of the event
+*@apiParam {string} couponName name of the coupon
+*
+*@apiParamExample {json} request-example
+*{
+*	"attendance": {
+*		"couponName":"dinner",
+*		"day":2,
+*		"email":"a@a.COM",
+*		"eventName":"DEVRELCONF"
+*	}
+*}
+*
+*@apiParamExample {json} response-example
+*{
+*    "rs": "No match found for this coupon",
+*    "err": null
+*}
+**/
 func (b *basicCouponsService) RedeemCoupon(ctx context.Context, attendance model.Attendance, couponName string) (rs string, err error) {
 	c := make(chan model.MessageReturn)
 	go model.RedeemCoupon(attendance, attendance.CouponName, c)
