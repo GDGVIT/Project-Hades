e31890e635af39a384f34df17112c3572dc1c8e3
L04DB4L4NC3R <angadsharma1016@gmail.com>
2019-06-02 02:17:25 +0530

    docs

--- doc/api_project.js
+++ doc/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-06-01T20:46:33.503Z",
+    "time": "2019-06-01T20:46:20.944Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
